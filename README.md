df388cd7e05b323bdea1887cf33faa868da42ce1
Alex <elvisavfc65@gmail.com>
2023-01-23 10:44:01 -0500

    doummented maximum_contact_radius.m

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 | :----:                     | :--------:       | :-----------: |
 | theta_from_cylindrical.m   |:white_check_mark:| :x:           |
 | r_from_spherical.m         | :x:              | :x:           |
-| maximum_contact_radius.m   | :x:              | :x:           |
+| maximum_contact_radius.m   |:white_check_mark:| :x:           |
 | zeta_generator.m           |:white_check_mark:| :x:           |
 | my_legendre.m              | :x:              | :x:           |
 | legendre_dx.m              | :x:              | :x:           |
--- matlab/1_code/maximum_contact_radius.m
+++ matlab/1_code/maximum_contact_radius.m
@@ -1,4 +1,28 @@
 function rmax = maximum_contact_radius(amplitudes)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% maximum_contact_radius.m - Calculates the maximum possible contact radius
+%
+% Given a dropplet described by the amplitudes, calculates the maximum
+% possible contact radius, given by the first time the dropplet has a
+% vertical tangent line. This maximum contact line is calculated using
+% Newton-Raphson.
+% The deformation is given by
+% f(angle) = 1 +  \sum_{i=1}^{N}  amplitudes(i) * Pl(cos(angle)))
+%
+% Arguments:
+% - amplitudes: One dimensional scalar array or STRUCT with field
+% "deformation_amplitudes"
+%
+% Outputs:
+% - rmax: Scalar value. 
+%
+% EXAMPLES
+% maximum_contact_radius([0, 0]);   % Returns 1
+%
+% Written by: Elvis Aguero- 01/01/2023
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
     if isstruct(amplitudes); amplitudes = amplitudes.deformation_amplitudes; end
     if size(amplitudes, 2) > 1; amplitudes = amplitudes'; end
     %collectdnPl = PROBLEM_CONSTANTS.collectdnPl;
