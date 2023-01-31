679ea72b68e4cafb9076b52e6cfbb8746951d693
br0nelvis <elvis_aguero@brown.edu>
2023-01-31 09:49:28 -0500

    implemented force only with B1

--- matlab/1_code/get_center_of_mass_test.m
+++ matlab/1_code/get_center_of_mass_test.m
@@ -1,5 +1,5 @@
-partial_results = zeros(8, 2);
-for ii = 1:8
+partial_results = zeros(9, 2);
+for ii = 1:9
     PROBLEM_CONSTANTS.CM = ii;
     [partial_results(ii, 1), partial_results(ii, 2)] = ...
         get_center_of_mass(new_amplitudes, new_velocities, probable_next_conditions, previous_conditions, PROBLEM_CONSTANTS);
